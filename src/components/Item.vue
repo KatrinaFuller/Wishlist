cead74143dd25d701425c8e2b60e754d3acbeaf4
Katrina Fuller <trinafuller@att.net>
2019-09-15 18:59:28 -0600

    started setting up home view

---
+++ src/components/Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item col-3 border rounded"></div>
+</template>
+
+
+<script>
+export default {
+  name: "item",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <div class="row">
+      <item v-for="item in items" :itemProp="item" :key="item._id" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Item from "../components/Item";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$store.dispatch("getItems");
+  },
+  methods: {},
+  computed: {
+    items() {
+      return this.$store.state.items;
+    }
+  },
+  components: { Item }
+};
 </script>
