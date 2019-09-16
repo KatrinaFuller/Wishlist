da65644f0b27fa37da23f9bc37957092b23ab212
Katrina Fuller <trinafuller@att.net>
2019-09-15 19:12:34 -0600

    added items to the store

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="item col-3 border rounded"></div>
+  <div class="item col-3 border rounded" @click="viewItem()">
+    <img :src="itemProp.imgUrl" />
+  </div>
 </template>
 
 
 <script>
 export default {
   name: "item",
+  props: ["itemProp"],
   data() {
     return {};
   },
   computed: {},
-  methods: {},
+  methods: {
+    viewItem() {
+      this.$router.push({
+        name: "item",
+        params: { itemId: this.itemProp._id }
+      });
+    }
+  },
   components: {}
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from './router'
+import axios from 'axios'
 
 Vue.use(Vuex)
+let api = axios.create({
+  baseURL: '//localhost:3000/api'
+})
 
 export default new Vuex.Store({
   state: {
-
+    items: []
   },
   mutations: {
-
+    setItems(state, payload) {
+      state.items = payload
+    }
   },
   actions: {
-
+    async getItems({ commit, dispatch }) {
+      try {
+        let res = await api.get('items')
+        commit('setItems', res.data)
+      } catch (error) {
+        console.error('store.js: getItems()')
+      }
+    }
   }
 })
